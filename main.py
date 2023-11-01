77be1f2b395a963f4caaa2154282b9dad38e75a0
StephenGarretJogie <stephen.garret.jogie@ansamcal.com>
2023-11-01 15:51:25 -0400

    updated the adding of values function to be more in line with the real data we would get and fixed some email formatting

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ import requests
 #from dotenv import load_dotenv
 import datetime
 
+#==============================================================================
+#===========================GLOBAL VARIABLES===================================
+#==============================================================================
 sender_email = "stephen.garret.jogie@gmail.com"
-receiver_email = "stephen.garret.jogie@ansamcal.com"
+receiver_email = r"stephen.garret.jogie@ansamcal.com;simeon.ramjit@ansamcal.com"
 username = "stephen.garret.jogie@gmail.com"
 password = input(f"enter password for {username}: ")
 smtp_server = "smtp.gmail.com"
@@ -52,11 +55,12 @@ def send_email(subject:str,message:str) -> None:
         print(f"Error: {e}")
     return
 
-def update_rect_values(rect : Rectifier, total, val_16k, val_24k) -> None:
+def update_rect_values(rect : Rectifier, val_16k : float, val_24k : float) -> None:
     temp = rect.no_load_state
-    rect.total_load = total
-    rect.sixteen_load = val_16k
-    rect.twenty_four_load = val_24k
+    rect.total_load = round(val_16k + val_24k,2)
+    # print(rect.total_load) # test line
+    rect.sixteen_load = round(val_16k,2)
+    rect.twenty_four_load = round(val_24k,2)
 
     if (rect.total_load > 2.00):
         rect.no_load_state = False
@@ -76,7 +80,7 @@ def update_rect_values(rect : Rectifier, total, val_16k, val_24k) -> None:
 def rectifier_no_load_alert(rectifier : Rectifier) -> None:
     timestamp = datetime.datetime.fromtimestamp(get_time_from_api().unix_time)
     rectifier_trip_message = f"Rectifer No Load Condition Detected {timestamp} \nTotal Load: {rectifier.total_load}kA \n16kA Rectifier Load: {rectifier.sixteen_load}kA \n24kA Rectifier Load: {rectifier.twenty_four_load}kA"
-    subject = subject = f"[Rectifier Trip]- {timestamp}"
+    subject = subject = f"[Rectifier No Load Condition]- {timestamp}"
     send_email(subject, rectifier_trip_message)
     return
 
@@ -91,14 +95,14 @@ def rectifier_restart_alert(rectifier : Rectifier) -> None:
 
 def test_thing() -> None:
     rect_1 = Rectifier() 
-    update_rect_values(rect_1, 4.5, 2.5,2) #test val
+    update_rect_values(rect_1, 2.5,2) #test val
     #rectifier_no_load_alert(rect_1)
-    update_rect_values(rect_1, 5, 2.5,2)
-    update_rect_values(rect_1, 1.5, 2.5,2)
-    update_rect_values(rect_1, 0.99, 2.5,2)
-    update_rect_values(rect_1, 1.5, 2.5,2)
-    update_rect_values(rect_1, 2.01, 2.5,2)
-    update_rect_values(rect_1, 4.5, 2.5,2)
+    update_rect_values(rect_1, 2.5,2.5)
+    update_rect_values(rect_1, 0.5,1)
+    update_rect_values(rect_1, 0.5,0)
+    update_rect_values(rect_1, 1.5,0.0)
+    update_rect_values(rect_1, 1.6,0.41)
+    update_rect_values(rect_1, 2.5,2)
     #rectifier_restart_alert(rect_1)
     
     return
